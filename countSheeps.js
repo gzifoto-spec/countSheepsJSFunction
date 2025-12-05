44646532a2811594c73eef0692feb140e7705996
gzifoto-spec <gzifoto@gmail.com>
2025-12-05 08:19:23 +0100

    feat: Function upload

---
+++ countSheeps.js
@@ -0,0 +1,17 @@
+
+function countSheeps(list) {
+    const sheepCount = list.filter(Boolean).length;
+    if (sheepCount === 0) {
+        console.log("UPS!!! Wolfs ate Sheeps");
+    } else {
+        console.log(`There are ${sheepCount} sheep in total`);
+    }
+}
+
+countSheeps([
+    true, true, true, false, true, true, true, true, true, false,
+    true, false, true, false, false, true, true, true, true, true,
+    false, false, true, true
+]);
+
+countSheeps([false, false, false]);
